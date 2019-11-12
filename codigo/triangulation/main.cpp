bbbe35f41066fb63d7b54d580f89ac68098b397a
aldo_bedrij <wbedrij@gmail.com>
2019-11-12 19:20:10 -0300

    triangulación delaunay

--- codigo/triangulation/main.cpp
+++ codigo/triangulation/main.cpp
@@ -8,10 +8,14 @@
 #include <pcl/io/ply_io.h>
 #include <pcl/point_cloud.h>
 
+#include <pcl/geometry/triangle_mesh.h>
+#include <delaunator.hpp>
+
 #include <pcl/visualization/pcl_visualizer.h>
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 pcl::PointCloud<pcl::PointXYZ>::Ptr load_cloud(std::string filename);
 void usage(const char *program) {
@@ -19,6 +23,15 @@ void usage(const char *program) {
 }
 
 pcl::PointCloud<pcl::PointXYZ>::Ptr project(pcl::PointCloud<pcl::PointXYZ>::Ptr nube);
+
+namespace nih{
+	struct VertexData{
+		int id;
+	};
+	using TMesh = pcl::geometry::TriangleMesh <pcl::geometry::DefaultMeshTraits<VertexData> >::Ptr;
+}
+
+nih::TMesh triangulate(pcl::PointCloud<pcl::PointXYZ>::Ptr nube);
 
 int main(int argc, char **argv) {
 	if(argc < 2) {
@@ -28,6 +41,8 @@ int main(int argc, char **argv) {
 
 	auto nube = load_cloud(argv[1]);
 	auto proyectada = project(nube->makeShared());
+
+	auto triangle_mesh = triangulate(nube);
 
 
 	//visualization
@@ -45,6 +60,38 @@ int main(int argc, char **argv) {
 	return 0;
 }
 
+nih::TMesh triangulate(pcl::PointCloud<pcl::PointXYZ>::Ptr nube) {
+	auto mesh = boost::make_shared<nih::TMesh::element_type>();
+
+	/*triangulación delaunay*/
+	// copiar las coordenadas xy de la nube de puntos
+	std::vector<double> xy;
+	xy.resize(2 * nube->size());
+	for(int K = 0; K < nube->size(); ++K) {
+		xy[2 * K] = (*nube)[K].x;
+		xy[2 * K + 1] = (*nube)[K].y;
+	}
+	// cálculo de la triangulación
+	delaunator::Delaunator delaunay(xy);
+	//índices de los vértices de los triángulos triangulos en
+	//delaunay.triangles[3*K+{0..2}]
+
+	// llenar la malla
+	// primero los vértices
+	mesh->reserveVertices(nube->size());
+	for(int K = 0; K < nube->size(); ++K)
+		mesh->addVertex(nih::VertexData{K});
+	// luego los triángulos
+	mesh->reserveFaces(delaunay.triangles.size());
+	for(int K = 0; K < delaunay.triangles.size(); K += 3)
+		mesh->addFace(
+		    pcl::geometry::VertexIndex(delaunay.triangles[K]),
+		    pcl::geometry::VertexIndex(delaunay.triangles[K + 1]),
+		    pcl::geometry::VertexIndex(delaunay.triangles[K + 2]));
+
+	return mesh;
+}
+
 pcl::PointCloud<pcl::PointXYZ>::Ptr load_cloud(std::string filename) {
 	pcl::PLYReader reader;
 	auto nube = boost::make_shared<pcl::PointCloud<pcl::PointXYZ> >();
@@ -56,7 +103,7 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr project(pcl::PointCloud<pcl::PointXYZ>::Ptr 
 	//z = 0 
 	//proyección ortogonal
 	
-	for(int K=0; K<nube->points.size(); ++K)
-		nube->points[K].z = 0;
+	for(int K=0; K<nube->size(); ++K)
+		(*nube)[K].z = 0;
 	return nube;
 }
