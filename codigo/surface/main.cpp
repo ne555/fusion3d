0c08013f030c982bcc2e982d8a17780b2b782974
aldo_bedrij <wbedrij@gmail.com>
2020-01-09 16:20:17 +0000

    visualizer, nubes a diferentes colores

--- codigo/surface/main.cpp
+++ codigo/surface/main.cpp
@@ -15,6 +15,8 @@ namespace nih {
 	cloud_with_normal load_cloud_normal(std::string filename);
 }
 
+void visualise(const std::vector<nih::cloud_with_normal> &nubes);
+
 int main(int argc, char **argv) {
 	if(argc < 3) {
 		usage(argv[0]);
@@ -26,13 +28,39 @@ int main(int argc, char **argv) {
 	std::ifstream input(config);
 	std::string filename;
 	std::vector<nih::cloud_with_normal> clouds;
+
+	std::cerr << "Loading clouds";
 	while(input >> filename) {
+		std::cerr << '.';
 		auto cloud_ = nih::load_cloud_normal(directory + filename);
 		cloud_.transformation_ = nih::get_transformation(input);
 		clouds.push_back(cloud_);
 	}
+	std::cerr << "\nLoad finished\n";
 
+	visualise(clouds);
 	return 0;
+}
+
+void visualise(const std::vector<nih::cloud_with_normal> &nubes){
+	auto view =
+	    boost::make_shared<pcl::visualization::PCLVisualizer>("surface");
+	view->setBackgroundColor(0, 0, 0);
+	double delta = 1./(nubes.size()-1);
+
+	std::cerr << "clouds\n";
+	for(size_t K = 0; K < nubes.size(); ++K) {
+		std::cerr << K << ' ' << nubes[K].points_->size() << '\n';
+		view->addPointCloud<nih::point>(nubes[K].points_, std::to_string(K));
+		view->setPointCloudRenderingProperties(
+			pcl::visualization::PCL_VISUALIZER_COLOR,
+			1,K*delta,0,
+			std::to_string(K)
+		);
+	}
+
+	while(!view->wasStopped())
+		view->spinOnce(100);
 }
 
 void usage(const char *program) {
