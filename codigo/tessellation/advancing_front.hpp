a9fa243be94b8bee211f8d410a6c58e676b17d68
aldo_bedrij <wbedrij@gmail.com>
2020-02-01 13:11:58 -0300

    construcción del octree

--- codigo/tessellation/advancing_front.hpp
+++ codigo/tessellation/advancing_front.hpp
@@ -3,7 +3,7 @@
 #define ADVANCING_FRONT_HPP
 
 namespace nih {
-	//no acepta islas
+	// no acepta islas
 	class advancing_front {
 	public:
 		inline advancing_front(
@@ -34,13 +34,31 @@ namespace nih {
 	      mesh_(mesh_),
 	      boundary_(boundary_),
 	      length_(cloud_resolution<pointnormal>(cloud_)),
-	      octree(length_),
-	      patch_(create<cloudnormal>()) {
-	}
+	      patch_(create<cloudnormal>()) {}
 
 	inline cloudnormal::Ptr advancing_front::tessellate(int index) {
 		patch_->clear();
+		// el proceso crea nuevos bordes
+		std::vector<pcl::Vertices> to_fill;
+		to_fill.push_back(boundary_[index]);
 
+		// para detectar puntos cercanos a los nuevos
+		pcl::octree::OctreePointCloudSearch<pointnormal> octree(length_);
+		octree.setInputCloud(patch_);
+		for(int K = 0; K < to_fill[0].vertices.size(); ++K) {
+			auto p = (*cloud_)[to_fill[0].vertices[K]];
+			octree.addPointToCloud(p, patch_);
+		}
+
+		for(int K = 0; K < to_fill.size(); ++K) {
+			auto &border = to_fill[K].vertices;
+			while(border.size() >= 3) {
+				auto [candidate, angle_] =
+				    smallest_angle(*cloud_, *mesh_, border);
+			}
+		}
+
+		boundary_[index] = to_fill[0];
 		return patch_;
 	}
 } // namespace nih
