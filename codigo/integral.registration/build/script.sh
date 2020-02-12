d2b9e650f19f78bcb41a94377dcf9b693a0913b9
aldo_bedrij <wbedrij@gmail.com>
2020-02-12 19:09:16 -0300

    con corrección de bucle

--- codigo/integral.registration/build/script.sh
+++ codigo/integral.registration/build/script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#sin corrección de bucle
-
 mkdir --parents result/ArmadilloBack.conf result/ArmadilloOnHeadMultiple.conf result/ArmadilloOnHeadMultipleOffset.conf result/ArmadilloStand.conf result/ArmadilloStandFlip.conf result/bun.conf result/dragonSideRight.conf result/dragonStandRight.conf result/dragonUpRight.conf result/drill_1.6mm_cyb.conf result/happyBackRight.conf result/happySideRight.conf result/happyStandRight.conf
 
 #ejecución
@@ -9,28 +7,28 @@ mkdir --parents result/ArmadilloBack.conf result/ArmadilloOnHeadMultiple.conf re
 
 ##bunny
 echo "bunny"
-time ./registrar database/bunny/data/ bun.conf sin_correcion_de_bucle
+time ./registrar database/bunny/data/ bun.conf
 
 #armadillo
 echo "armadillo"
 for K in {ArmadilloBack.conf,ArmadilloOnHeadMultiple.conf,ArmadilloOnHeadMultipleOffset.conf,ArmadilloStand.conf,ArmadilloStandFlip.conf}; do
-	time ./registrar database/Armadillo_scans/ "$K" sin_correcion_de_bucle
+	time ./registrar database/Armadillo_scans/ "$K"
 done
 
 ##dragon
 #dragonSideRight.conf, dragonStandRight.conf, dragonUpRight.conf
 echo "dragon"
-time ./registrar database/dragon/dragon_side/ dragonSideRight.conf sin_correcion_de_bucle
-time ./registrar database/dragon/dragon_stand/ dragonStandRight.conf sin_correcion_de_bucle
-time ./registrar database/dragon/dragon_up/ dragonUpRight.conf sin_correcion_de_bucle
+time ./registrar database/dragon/dragon_side/ dragonSideRight.conf
+time ./registrar database/dragon/dragon_stand/ dragonStandRight.conf
+time ./registrar database/dragon/dragon_up/ dragonUpRight.conf
 
 ##drill
 echo "drill"
-time ./registrar database/drill/data/ drill_1.6mm_cyb.conf sin_correcion_de_bucle
+time ./registrar database/drill/data/ drill_1.6mm_cyb.conf
 
 ##happy
 #happyBackRight.conf, happySideRight.conf, happyStandRight.conf
 echo "happy"
-time ./registrar database/happy/happy_back/ happyBackRight.conf sin_correcion_de_bucle
-time ./registrar database/happy/happy_side/ happySideRight.conf sin_correcion_de_bucle
-time ./registrar database/happy/happy_stand/ happyStandRight.conf sin_correcion_de_bucle
+time ./registrar database/happy/happy_back/ happyBackRight.conf
+time ./registrar database/happy/happy_side/ happySideRight.conf
+time ./registrar database/happy/happy_stand/ happyStandRight.conf
