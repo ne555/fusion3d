9b6d47ac32b7a0e32c24ae6dc81893de868a6e0c
aldo_bedrij <wbedrij@gmail.com>
2020-02-14 10:27:50 -0300

    lee una sola

--- codigo/show_transformation/main.cpp
+++ codigo/show_transformation/main.cpp
@@ -5,9 +5,6 @@
 
 int main(){
 	std::ifstream input("/dev/stdin");
-	auto cybcyl = nih::get_transformation(input);
 	auto first = nih::get_transformation(input);
-
-	nih::show_transformation(cybcyl, std::cout);
 	nih::show_transformation(first, std::cout);
 }
