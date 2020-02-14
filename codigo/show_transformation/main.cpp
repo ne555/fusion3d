69b8b338fa401ec050d70dbf2a11575e6b81ef66
aldo_bedrij <wbedrij@gmail.com>
2020-02-14 15:42:03 -0300

    lee varias transformaciones (empiezan por filename)

--- codigo/show_transformation/main.cpp
+++ codigo/show_transformation/main.cpp
@@ -3,8 +3,18 @@
 #include "fusion_3d.hpp"
 #include "functions.hpp"
 
-int main(){
-	std::ifstream input("/dev/stdin");
-	auto first = nih::get_transformation(input);
-	nih::show_transformation(first, std::cout);
+int main(int argc, char **argv) {
+	std::string in;
+	if(argc == 1)
+		in = "/dev/stdin";
+	else
+		in = argv[1];
+	std::ifstream input(in);
+	std::string filename;
+	while(input >> filename) {
+		auto first = nih::get_transformation(input);
+		std::cout << filename << '\n';
+		std::cout << first.matrix() << '\n';
+		nih::show_transformation(first, std::cout);
+	}
 }
